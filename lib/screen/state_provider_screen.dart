915a6678560939d43a0c70d669ae8f1040ddd92f
rldnd <gi981226@gmail.com>
2023-05-08 13:26:43 +0900

    feat(stateProvider): update second methods

--- lib/screen/state_provider_screen.dart
+++ lib/screen/state_provider_screen.dart
@@ -25,6 +25,13 @@ class StateProviderScreen extends ConsumerWidget {
             ),
             ElevatedButton(
               onPressed: () {
+                ref.read(numberProvider.notifier).state =
+                    ref.read(numberProvider.notifier).state - 1;
+              },
+              child: const Text('DOWN'),
+            ),
+            ElevatedButton(
+              onPressed: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
                     builder: (_) => _NextScreen(),
