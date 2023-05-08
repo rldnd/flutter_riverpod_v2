d7fdc6b17cfb59ddd64a3f104193e433ba856745
rldnd <gi981226@gmail.com>
2023-05-08 17:31:35 +0900

    feat(provider): provider observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod_v2/riverpod/provider_observer.dart';
 import 'package:flutter_riverpod_v2/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: MaterialApp(
+    ProviderScope(
+      observers: [Logger()],
+      child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomeScreen(),
       ),
---
+++ lib/riverpod/provider_observer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Logger extends ProviderObserver {
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    super.didUpdateProvider(provider, previousValue, newValue, container);
+  }
+
+  @override
+  void didAddProvider(
+    ProviderBase<Object?> provider,
+    Object? value,
+    ProviderContainer container,
+  ) {
+    // TODO: implement didAddProvider
+    super.didAddProvider(provider, value, container);
+  }
+}
